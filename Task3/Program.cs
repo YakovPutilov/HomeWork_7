2d3b46f8d17294001dba56a57d295398c9d2c3d3
Yakov Putiliov <jack27@ya.ru>
2023-12-11 13:14:20 +0300

    Complite Task3

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -2,4 +2,12 @@
 // Выведете его элементы, начиная с конца. 
 // Использовать рекурсию, не использовать циклы.
 
+void ReverseArray( int [] arr, int i){
+    if (i<arr.Length){
+        ReverseArray(arr, i+1);
+        Console.Write(arr[i] + " ");
+    }
+}
 
+int [] array = {1, 2, 5, 0, 10, 34};
+ReverseArray(array, 0);
