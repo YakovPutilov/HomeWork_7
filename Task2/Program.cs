66bb26ecab2624be8a6a08edd5e3e0ff5f7e5c8b
Yakov Putiliov <jack27@ya.ru>
2023-12-11 12:51:19 +0300

    Complite Task2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿    // Задача 2: Напишите программу вычисления функции Аккермана 
+    // с помощью рекурсии. Даны два неотрицательных числа m и n.
+
+//     функция ack(n, m)
+//    если n = 0
+//      вернуть m + 1
+//    иначе, если m = 0
+//      вернуть ack (n - 1, 1)
+//    еще
+//      вернуть ack(n - 1, ack (n, m - 1))
+    
+int AcMan(int m, int n){
+    if(m == 0) return n + 1;
+    else{
+        if(n == 0){
+            return AcMan(m - 1, 1);
+        }
+        else{
+            return AcMan(m - 1, AcMan(m, n - 1));
+        }
+    }
+
+}
+
+int result = AcMan(3,2);
+Console.WriteLine(result);
+
