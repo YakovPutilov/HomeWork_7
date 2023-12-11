e65ea48b176460578e2636eae25b57417fcecff3
Yakov Putiliov <jack27@ya.ru>
2023-12-11 12:26:56 +0300

    Complite Task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 1: Задайте значения M и N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке от M до N. 
+// Использовать рекурсию, не использовать циклы.
+
+Console.WriteLine("Enter N");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter M");
+int m = Convert.ToInt32(Console.ReadLine());
+
+void NaturalNumbers(int n, int m){
+    if (m > n) {
+        NaturalNumbers(n,m - 1);
+        Console.Write(m + " ");
+    }
+    else if(m < n){
+        Console.WriteLine("N must be graiter then M");
+    }    
+}
+
+
+NaturalNumbers(n,m);
